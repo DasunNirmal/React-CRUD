2b983f7e6cbab0d023e9f5c9e4e9c5a108fe69a8
DasunNirmal <dasun200301@gmail.com>
2024-12-23 13:58:10 +0530

    updated

--- src/component/Navigation.tsx
+++ src/component/Navigation.tsx
@@ -1,19 +1,19 @@
-import {Link} from "react-router";
-
+import { Link } from "react-router";
+import "./Navigation.css";
 
 export function Navigation() {
     return (
-        <>
-            <header className="navbar">
-                <nav className="navbar-list">
-                    <ul className=".navbar-item">
-                        <Link className='navbar-link' to=''>Dashboard</Link>
-                        <Link className='navbar-link' to='/add'>Add Customer</Link>
-                        <Link className='navbar-link' to='/update'>Update Customer</Link>
-                        <Link className='navbar-link' to='/delete'>Delete Customer</Link>
+        <div className="px-3 py-2 h-20 bg-sky-950 flex items-center">
+            <header className="px-4 py-3">
+                <nav className="flex space-x-4 justify-center">
+                    <ul className="flex space-x-4 text-white">
+                        <li><Link className="custom-link" to=''>Dashboard</Link></li>
+                        <li><Link className="custom-link" to='/add'>Add Customer</Link></li>
+                        <li><Link className="custom-link" to='/update'>Update Customer</Link></li>
+                        <li><Link className="custom-link" to='/delete'>Delete Customer</Link></li>
                     </ul>
                 </nav>
             </header>
-        </>
-    )
+        </div>
+    );
 }
