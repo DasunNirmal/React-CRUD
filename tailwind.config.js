bd458918658c492906381aec9e428f9367a3bfc4
DasunNirmal <dasun200301@gmail.com>
2024-12-23 17:08:29 +0530

    done

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,10 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {}
+    extend: {fontFamily: {
+        sans: ['Poppins', 'sans-serif'], // Default sans-serif font family will be Poppins
+      },
+    }
   },
   plugins: []
 }
