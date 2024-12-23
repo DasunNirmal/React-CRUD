b70948b7ccf4590fca27cd7920129e8089c04e16
DasunNirmal <dasun200301@gmail.com>
2024-12-23 20:03:30 +0530

    items and customer cards done

--- src/pages/UpdateCustomer.tsx
+++ src/pages/UpdateCustomer.tsx
@@ -20,19 +20,73 @@ export default function UpdateCustomer() {
     }
 
     return (
-        <>
-            <h2>Update Customer</h2>
-            <input className="inputCSS" type="text" placeholder="Name"
-                   onChange={(e) => setName(e.target.value)}/><br/><br/>
-            <input className="inputCSS" type="text" placeholder="Email"
-                   onChange={(e) => setEmail(e.target.value)}/><br/><br/>
-            <input className="inputCSS" type="text" placeholder="Phone Number"
-                   onChange={(e) => setPhone(e.target.value)}/>
-            <br/><br/>
-            {customers.map((customer: Customer) => (
-                <div
-                    key={customer.email}>{customer.name + ' | ' + customer.email + ' | ' + customer.phone}</div>))}<br/>
-            <button className="buttonCSS" onClick={updateCustomer}>Update Customer</button>
-        </>
+        <div className="grid grid-cols-2 main-section p-6">
+            {/*customer section*/}
+            <div className="left-card m-3">
+                <h2 className="mb-6 p-2 w-fit text-2xl">Update Customer</h2>
+
+                <input className="mb-6 p-2" type="text" placeholder="Name"
+                       onChange={(e) => setName(e.target.value)}/>
+                <input className="mb-6 p-2" type="text" placeholder="Email"
+                       onChange={(e) => setEmail(e.target.value)}/>
+                <input className="mb-6 p-2" type="text" placeholder="Phone Number"
+                       onChange={(e) => setPhone(e.target.value)}/>
+                <button className="buttonCSS" onClick={updateCustomer}>Update Customer</button>
+
+                <table className="table-auto border border-gray-300 w-full mt-6">
+                    <thead>
+                    <tr>
+                        <td>Name</td>
+                        <td>Email</td>
+                        <td>phone</td>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    {customers.map((customer: Customer) => (
+                        <tr key={customer.email}>
+                            <td>{customer.name}</td>
+                            <td>{customer.email}</td>
+                            <td>{customer.phone}</td>
+                        </tr>
+                    ))}
+                    </tbody>
+                </table>
+            </div>
+
+            {/*item section*/}
+            <div className="right-card m-3">
+                <h2 className="mb-6 p-2 w-fit text-2xl">Update Items</h2>
+
+                <input className="mb-6 p-2" type="text" placeholder="Item Name"
+                />
+                <input className="mb-6 p-2" type="text" placeholder="Item"
+                />
+                <input className="mb-6 p-2" type="text" placeholder="Item"
+                />
+                <button className="buttonCSS">Add Item</button>
+
+                <table className="table-auto border border-gray-300 w-full mt-6">
+                    <thead>
+                    <tr>
+                        <td>Name</td>
+                        <td>Email</td>
+                        <td>phone</td>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    {/*{customers.map((customer: Customer) => (
+                        <tr key={customer.email}>
+                            <td>{customer.name}</td>
+                            <td>{customer.email}</td>
+                            <td>{customer.phone}</td>
+                        </tr>
+                    ))}*/}
+                    <tr>
+
+                    </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
     )
 }
