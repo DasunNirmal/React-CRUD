06bec88baf3ef352f7448e3f9f2af0fc60640753
DasunNirmal <dasun200301@gmail.com>
2024-12-23 13:57:19 +0530

    updated

--- src/component/CustomerProvider.tsx
+++ src/component/CustomerProvider.tsx
@@ -1,5 +1,5 @@
 import {createContext, useState} from "react";
-import {Customer} from "../../../react-intro/src/component/Customer.tsx";
+import {Customer} from "./Customer.ts";
 
 export const CustomerContext = createContext();
 
