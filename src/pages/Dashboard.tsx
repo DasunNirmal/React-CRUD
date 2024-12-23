f6b41e8ef5805b129152c0fab283e9e9b1b9b0aa
DasunNirmal <dasun200301@gmail.com>
2024-12-23 20:03:15 +0530

    items and customer cards done

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -5,10 +5,57 @@ import {Customer} from "../component/Customer.ts";
 export default function Dashboard() {
     const [customers, setCustomers] = useContext(CustomerContext);
     return (
-        <>
-            <h2>Dash Board Component</h2>
-            {customers.map((customer: Customer) => (
-                <div key={customer.email}>{customer.name + ' | ' + customer.email + ' | ' + customer.phone}</div>))}
-        </>
+        <div className="grid grid-cols-2 main-section p-6">
+
+            {/*customer section*/}
+            <div className="left-card m-3">
+                <h2 className="mb-6 p-2 w-fit text-2xl">Customers</h2>
+                <table className="table-auto border border-gray-300 w-full mt-6">
+                    <thead>
+                    <tr>
+                        <td>Name</td>
+                        <td>Email</td>
+                        <td>phone</td>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    {customers.map((customer: Customer) => (
+                        <tr key={customer.email}>
+                            <td>{customer.name}</td>
+                            <td>{customer.email}</td>
+                            <td>{customer.phone}</td>
+                        </tr>
+                    ))}
+                    </tbody>
+                </table>
+            </div>
+
+            {/*item section*/}
+            <div className="right-card m-3">
+                <h2 className="mb-6 p-2 w-fit text-2xl">Items</h2>
+
+                <table className="table-auto border border-gray-300 w-full mt-6">
+                    <thead>
+                    <tr>
+                        <td>Name</td>
+                        <td>Email</td>
+                        <td>phone</td>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    {/*{customers.map((customer: Customer) => (
+                        <tr key={customer.email}>
+                            <td>{customer.name}</td>
+                            <td>{customer.email}</td>
+                            <td>{customer.phone}</td>
+                        </tr>
+                    ))}*/}
+                    <tr>
+
+                    </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
     )
 }
