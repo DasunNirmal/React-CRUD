b45c3fc40ae66693599daa0fec1ecbef14413e43
DasunNirmal <dasun200301@gmail.com>
2024-12-23 20:03:23 +0530

    items and customer cards done

--- src/pages/DeleteCustomer.tsx
+++ src/pages/DeleteCustomer.tsx
@@ -18,15 +18,72 @@ export default function DeleteCustomer() {
     }
 
     return (
-        <>
-            <h2>Delete Customer</h2><br/>
-            <input className="inputCSS" type="text" placeholder="Email"
-                   onChange={(e) => setEmail(e.target.value)}/><br/><br/>
-            {customers.map((customer: Customer) => (
-                <div
-                    key={customer.email}>{customer.name + ' | ' + customer.email + ' | ' + customer.phone}</div>))}<br/>
-            <button className="buttonCSS" onClick={deleteCustomer}>Delete Customer</button>
-            <button className="buttonCSS" onClick={deleteCustomerByEmail}>Delete By Email</button>
-        </>
+        <div className="grid grid-cols-2 main-section p-6">
+
+            {/*customer section*/}
+            <div className="left-card m-3">
+                <h2 className="mb-6 p-2 w-fit text-2xl">Delete Customer</h2>
+
+                <input className="mb-6 p-2" type="text" placeholder="Email"
+                       onChange={(e) => setEmail(e.target.value)}/>
+                <div className="flex w-3/4 justify-center gap-10">
+                    <button className="w-2/4" onClick={deleteCustomer}>Delete Last Customer</button>
+                    <button className="w-2/4" onClick={deleteCustomerByEmail}>Delete By Email</button>
+                </div>
+
+                <table className="table-auto border border-gray-300 w-full mt-6">
+                    <thead>
+                    <tr>
+                        <td>Name</td>
+                        <td>Email</td>
+                        <td>phone</td>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    {customers.map((customer: Customer) => (
+                        <tr key={customer.email}>
+                            <td>{customer.name}</td>
+                            <td>{customer.email}</td>
+                            <td>{customer.phone}</td>
+                        </tr>
+                    ))}
+                    </tbody>
+                </table>
+            </div>
+
+            {/*item section*/}
+            <div className="right-card m-3">
+                <h2 className="mb-6 p-2 w-fit text-2xl">Update Items</h2>
+
+                <input className="mb-6 p-2" type="text" placeholder="Item Code"
+                />
+                <div className="flex w-3/4 justify-center gap-10">
+                    <button className="w-2/4">Delete Last Item</button>
+                    <button className="w-2/4">Delete By Item Code</button>
+                </div>
+
+                <table className="table-auto border border-gray-300 w-full mt-6">
+                    <thead>
+                    <tr>
+                        <td>Name</td>
+                        <td>Email</td>
+                        <td>phone</td>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    {/*{customers.map((customer: Customer) => (
+                        <tr key={customer.email}>
+                            <td>{customer.name}</td>
+                            <td>{customer.email}</td>
+                            <td>{customer.phone}</td>
+                        </tr>
+                    ))}*/}
+                    <tr>
+
+                    </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
     )
 }
