0b6a189b478974b466dfaa68582ec59f01444315
DasunNirmal <dasun200301@gmail.com>
2024-12-23 21:19:46 +0530

    small changes

--- src/component/Data.tsx
+++ src/component/Data.tsx
@@ -2,11 +2,6 @@ import {useContext, useState} from "react";
 import {CustomerContext} from "../store/CustomerProvider.tsx";
 
 export const [customers, setCustomers] = useContext(CustomerContext);
-/*
-const [name, setName] = useState("");
-const [email, setEmail] = useState("");
-const [phone, setPhone] = useState("");
-*/
 
 export const [state, setState] = useState({
     name: "",
