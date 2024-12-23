dccda22de4ca02b5a6157bf98ff2ad01e528d94b
DasunNirmal <dasun200301@gmail.com>
2024-12-23 17:08:22 +0530

    done

--- src/pages/AddCustomer.tsx
+++ src/pages/AddCustomer.tsx
@@ -1,11 +1,9 @@
 import {useContext, useState} from "react";
 import {CustomerContext} from "../component/CustomerProvider.tsx";
 import {Customer} from "../component/Customer.ts";
-import {useNavigate} from "react-router";
 import "./AddCustomer.css"
 
 export default function AddCustomer() {
-    const navigate = useNavigate();
     const [customers, setCustomers] = useContext(CustomerContext);
     const [name, setName] = useState("");
     const [email, setEmail] = useState("");
@@ -14,34 +12,72 @@ export default function AddCustomer() {
     function addCustomer() {
         const newCustomer = new Customer(name, email, phone);
         setCustomers((customers: Customer[]) => [...customers, newCustomer]);
-        navigate('/')
     }
 
     return (
         <div className="grid grid-cols-2 main-section p-6">
-                {/*customer section*/}
-                <div className="left-card m-3">
-                    <h2>Add Customer</h2>
+            {/*customer section*/}
+            <div className="left-card m-3">
+                <h2 className="mb-6 p-2 w-fit text-2xl">Manage Customers</h2>
 
-                    <input className="inputCSS" type="text" placeholder="Name"
-                           onChange={(e) => setName(e.target.value)}/>
-                    <input className="inputCSS" type="text" placeholder="Email"
-                           onChange={(e) => setEmail(e.target.value)}/>
-                    <input className="inputCSS" type="text" placeholder="Phone Number"
-                           onChange={(e) => setPhone(e.target.value)}/>
-                    <button className="buttonCSS" onClick={addCustomer}>Add Customer</button>
-                </div>
+                <input className="mb-6 p-2" type="text" placeholder="Name"
+                       onChange={(e) => setName(e.target.value)}/>
+                <input className="mb-6 p-2" type="text" placeholder="Email"
+                       onChange={(e) => setEmail(e.target.value)}/>
+                <input className="mb-6 p-2" type="text" placeholder="Phone Number"
+                       onChange={(e) => setPhone(e.target.value)}/>
+                <button className="buttonCSS" onClick={addCustomer}>Add Customer</button>
+
+                <table className="table-auto border border-gray-300 w-full mt-6">
+                    <thead>
+                    <tr>
+                        <td>Name</td>
+                        <td>Email</td>
+                        <td>phone</td>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    {customers.map((customer: Customer) => (
+                        <tr key={customer.email}>
+                            <td>{customer.name}</td>
+                            <td>{customer.email}</td>
+                            <td>{customer.phone}</td>
+                        </tr>
+                    ))}
+                    </tbody>
+                </table>
+            </div>
+
             {/*item section*/}
             <div className="right-card m-3">
-                <h2>Add Item</h2>
+                <h2 className="mb-6 p-2 w-fit text-2xl">Manage Items</h2>
 
-                <input className="inputCSS" type="text" placeholder="Item Name"
+                <input className="mb-6 p-2" type="text" placeholder="Item Name"
                        onChange={(e) => setName(e.target.value)}/>
-                <input className="inputCSS" type="text" placeholder="Item"
+                <input className="mb-6 p-2" type="text" placeholder="Item"
                        onChange={(e) => setEmail(e.target.value)}/>
-                <input className="inputCSS" type="text" placeholder="Item"
+                <input className="mb-6 p-2" type="text" placeholder="Item"
                        onChange={(e) => setPhone(e.target.value)}/>
                 <button className="buttonCSS" onClick={addCustomer}>Add Item</button>
+
+                <table className="table-auto border border-gray-300 w-full mt-6">
+                    <thead>
+                    <tr>
+                        <td>Name</td>
+                        <td>Email</td>
+                        <td>phone</td>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    {customers.map((customer: Customer) => (
+                        <tr key={customer.email}>
+                            <td>{customer.name}</td>
+                            <td>{customer.email}</td>
+                            <td>{customer.phone}</td>
+                        </tr>
+                    ))}
+                    </tbody>
+                </table>
             </div>
         </div>
     )
