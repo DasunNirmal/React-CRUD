12dac5d12c66aecd65c528ff1a6caa934aec0a1c
DasunNirmal <dasun200301@gmail.com>
2024-12-24 20:40:28 +0530

    Items Can Be Saved

--- src/pages/Save.tsx
+++ src/pages/Save.tsx
@@ -1,16 +1,28 @@
 import {useContext, useState} from "react";
 import {CustomerContext} from "../store/CustomerProvider.tsx";
 import {Customer} from "../component/Customer.ts";
+import {ItemContext} from "../store/ItemProvider.tsx";
+import {Item} from "../component/Item.ts";
 
 export default function Save() {
     const [customers, setCustomers] = useContext(CustomerContext);
+    const [items, setItems] = useContext(ItemContext);
     const [name, setName] = useState("");
     const [email, setEmail] = useState("");
     const [phone, setPhone] = useState("");
 
+    const [code, setCode] = useState("");
+    const [itemName, setItemName] = useState("");
+    const [qty, setQty] = useState("");
+
     function addCustomer() {
         const newCustomer = new Customer(name, email, phone);
         setCustomers((customers: Customer[]) => [...customers, newCustomer]);
+    }
+
+    function addItem() {
+        const newItem = new Item(code, itemName, Number(qty));
+        setItems((items: Item[]) => [...items, newItem]);
     }
 
     return (
@@ -52,12 +64,12 @@ export default function Save() {
                 <h2 className="mb-6 p-2 w-fit text-2xl">Save Items</h2>
 
                 <input className="mb-6 p-2" type="text" placeholder="Item COde"
-                       />
+                      onChange={(e) => setCode(e.target.value)} />
                 <input className="mb-6 p-2" type="text" placeholder="Item Name"
-                       />
+                      onChange={(e) => setItemName(e.target.value)} />
                 <input className="mb-6 p-2" type="text" placeholder="Item Quantity"
-                       />
-                <button>Add Item</button>
+                      onChange={(e) => setQty(e.target.value)} />
+                <button onClick={addItem}>Add Item</button>
 
                 <table className="table-auto border border-gray-300 w-full mt-6">
                     <thead>
@@ -68,13 +80,13 @@ export default function Save() {
                     </tr>
                     </thead>
                     <tbody>
-                    {/*{items.map((items: Item) => (
+                    {items.map((items: Item) => (
                         <tr key={items.code}>
                             <td>{items.code}</td>
                             <td>{items.itemName}</td>
                             <td>{items.qty}</td>
                         </tr>
-                    ))}*/}
+                    ))}
                     <tr>
 
                     </tr>
