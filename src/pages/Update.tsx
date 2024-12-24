4868394dc4ff0f20bce7a46f9528a6fcf33b81d6
DasunNirmal <dasun200301@gmail.com>
2024-12-24 21:37:27 +0530

    small changes

--- src/pages/Update.tsx
+++ src/pages/Update.tsx
@@ -1,22 +1,31 @@
-import {useNavigate} from "react-router";
 import {useContext, useState} from "react";
 import {CustomerContext} from "../store/CustomerProvider.tsx";
 import {Customer} from "../component/Customer.ts";
+import {ItemContext} from "../store/ItemProvider.tsx";
+import {Item} from "../component/Item.ts";
 
 export default function Update() {
 
-    const navigate = useNavigate();
     const [customers, setCustomers] = useContext(CustomerContext);
+    const [items, setItems] = useContext(ItemContext);
     const [name, setName] = useState("");
     const [email, setEmail] = useState("");
     const [phone, setPhone] = useState("");
 
+    const [code, setCode] = useState("");
+    const [itemName, setItemName] = useState("");
+    const [qty, setQty] = useState("");
+
     function updateCustomer() {
         const updatedCustomer = customers.map((customers:Customer) => customers.email === email ?
             {...customers, name:name,email:email,phone:phone} : customers);
-        console.log(updatedCustomer);
         setCustomers(updatedCustomer)
-        navigate('/')
+    }
+
+    function updateItems() {
+        const updatedItem = items.map((items:Item) => items.code === code ?
+            {...items, code:code,itemName:itemName,qty:qty} : items);
+        setItems(updatedItem)
     }
 
     return (
@@ -57,33 +66,30 @@ export default function Update() {
             <div className="right-card m-3">
                 <h2 className="mb-6 p-2 w-fit text-2xl">Update Items</h2>
 
+                <input className="mb-6 p-2" type="text" placeholder="Item COde"
+                       onChange={(e) => setCode(e.target.value)} />
                 <input className="mb-6 p-2" type="text" placeholder="Item Name"
-                />
-                <input className="mb-6 p-2" type="text" placeholder="Item"
-                />
-                <input className="mb-6 p-2" type="text" placeholder="Item"
-                />
-                <button className="buttonCSS">Add Item</button>
+                       onChange={(e) => setItemName(e.target.value)} />
+                <input className="mb-6 p-2" type="text" placeholder="Item Quantity"
+                       onChange={(e) => setQty(e.target.value)} />
+                <button onClick={updateItems}>Update Item</button>
 
                 <table className="table-auto border border-gray-300 w-full mt-6">
                     <thead>
                     <tr>
-                        <td>Name</td>
-                        <td>Email</td>
-                        <td>phone</td>
+                        <td>Item Code</td>
+                        <td>Item Name</td>
+                        <td>Quantity</td>
                     </tr>
                     </thead>
                     <tbody>
-                    {/*{customers.map((customer: Customer) => (
-                        <tr key={customer.email}>
-                            <td>{customer.name}</td>
-                            <td>{customer.email}</td>
-                            <td>{customer.phone}</td>
+                    {items.map((items: Item) => (
+                        <tr key={items.code}>
+                            <td>{items.code}</td>
+                            <td>{items.itemName}</td>
+                            <td>{items.qty}</td>
                         </tr>
-                    ))}*/}
-                    <tr>
-
-                    </tr>
+                    ))}
                     </tbody>
                 </table>
             </div>
